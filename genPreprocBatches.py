984aead83b216d6128c347436ab8643e18a7b250
Alex Storer <alexstorer@gmail.com>
2012-05-15 11:52:55 -0400

    First version of making preprocessing batch files.  More to do!

---
+++ genPreprocBatches.py
@@ -0,0 +1,72 @@
+import argparse
+import glob,re
+
+
+def getNumRuns(spath,s):
+    print spath+'/'+s+'/preproc'
+    fruns = glob.glob(spath+'/'+s+'/preproc/*run*')
+    maxruns = 0    
+    for fname in fruns:
+        m = re.match('.*run(\d+)',fname)
+        runnum = int(m.group(1))
+        if runnum>maxruns:
+            maxruns = runnum
+    return maxruns
+
+
+def getNumPts(spath,s):
+    print spath+'/'+s+'/preproc'
+    fruns = glob.glob(spath+'/'+s+'/preproc/*run*')
+    nPts = reduce(max,map(lambda x: int(re.search('run\d*-(\d+)',x).group(1)),fruns))
+    nRuns = getNumRuns(spath,s)
+    for i = range(1,nRuns+1)
+    
+# parse the arguments
+
+parser = argparse.ArgumentParser(description='klasdfkjasdf')
+parser.add_argument('-t','--template', help='Template batch file', required=True)
+parser.add_argument('-p','--path', help='Path to subjects', required=True, nargs=1)
+parser.add_argument('-s','--subjects', help='List of subjects', required=False, nargs='*')
+parser.add_argument('-f','--subjectfile', help='File containing subjects', required=False)
+args = vars(parser.parse_args())
+
+print args
+
+f = open(args["template"])
+origtemplate = f.read()
+f.close()
+
+
+spath = args["path"][0]
+
+if args["subjects"]:
+    sublist = args["subjects"]
+elif args["subjectfile"]:
+    print "do this later"
+
+runList = re.findall('.*run(\d+)',origtemplate)
+nOrigRuns = reduce(max,map(int,runList))
+epiList = re.findall('.*run\d+-(\d+)',origtemplate)
+nPts = reduce(max,map(int,epiList))
+origPath = re.findall('(/.*)/preproc/',origtemplate)[0]
+
+
+
+errorlog = ''
+
+for s in sublist:
+    destPath = spath+'/'+s
+    subRuns = getNumRuns(spath,s)
+    subPts = getNumPts(spath,s)
+    if subRuns != nOrigRuns:
+        errorlog+="Subject "+ s +" has " + str(subRuns) + " runs, not " + str(nOrigRuns) + " runs.\n"
+        break
+    if subPts != nOrigRuns:
+        errorlog+="Subject "+ s +" has " + str(subPts) + " points, not " + str(nPts) + " points.\n"
+        break
+    nextBatch = origtemplate
+    nextBatch = nextBatch.replace(origPath,destPath)
+    fout = open(destPath+'/batch/preproc.m','w')
+    fout.write(nextBatch)
+    fout.close()
+
