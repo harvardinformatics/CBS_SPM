d6d5eb568756d7e2f7e37d2797dbfe0ccb295f2c
Alex Storer <alexstorer@gmail.com>
2012-07-06 11:01:45 -0400

    Error testing for earlier versions of python

--- genPreprocBatches.py
+++ genPreprocBatches.py
@@ -1,3 +1,15 @@
+#/usr/bin/env python
+
+import platform
+pstr = platform.python_version()
+if pstr[0]!="2":
+    print "This script is designed for Python 2.  Other versions may cause errors!"
+elif int(pstr[2])<7:
+    print '''This script has been tested in version 2.7 of python.  You are using an earlier version!  Please use the default environment as specified on the CBS FAQ page:
+http://cbs.fas.harvard.edu/science/core-facilities/neuroimaging/information-investigators/faq
+'''
+
+
 import argparse
 import glob,re,os.path,datetime,os
 
@@ -45,7 +57,6 @@ elif args["subjectfile"]:
     for l in fsf:        
         sublist.append(l.strip())
     fsf.close()
-    print sublist
 
 f = open(args["template"])
 origtemplate = f.read()
