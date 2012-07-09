e0561c45956f2a9a9ce3d40ccbfac66a4762732a
Alex Storer <alexstorer@gmail.com>
2012-07-09 16:48:12 -0400

    Escaped \n

--- getSubjectSPM.py
+++ getSubjectSPM.py
@@ -21,7 +21,7 @@ args = vars(parser.parse_args())
 #             cbsget, but you do have the dicoms already unzipped in a folder.
 
 # getsubject(subjectid,boldruns,structrun,fmruns,destpath,varargin)
-runcmd = 'matlab -nodisplay -r "try \ngetsubject('
+runcmd = 'matlab -nodisplay -r "try \\ngetsubject('
 runcmd += "\'"+args["subjectid"][0]+"\'"
 runcmd += ','
 runcmd += '['+''.join([b+',' for b in args["boldruns"]])[0:-1]+']'
@@ -36,10 +36,10 @@ runcmd += ','
 runcmd += "\'"+args["destpath"][0]+"\'"
 if args["dicompath"]:
     runcmd += ",\'"+args["dicompath"][0]+"\'"
-runcmd += ')"'
-runcmd += '\ncatch\n'
-runcmd += "exit()\n"
-runcmd += "end"
+runcmd += ')'
+runcmd += '\\ncatch\\n'
+runcmd += "exit()\\n"
+runcmd += "end""
 
 
 print runcmd
