b085a14c8f1b20019e35ab06701cf5ae7cc8fbd1
Stephanie McMains <mcmains@ncfws01.rc.fas.harvard.edu>
2012-08-03 15:00:44 -0400

    changed flags for consistency, updates to readme

--- genL1ArtBatches.py
+++ genL1ArtBatches.py
@@ -3,12 +3,12 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description='Update the Level 1 Analysis batches after running ART.')
-parser.add_argument('-b','--basedir', help='the path containing the subject directories (e.g. /users/me/data)', required=True, nargs=1)
+parser.add_argument('-p','--path', help='the path containing the subject directories (e.g. /users/me/data)', required=True, nargs=1)
+parser.add_argument('-t','--template', help='The name of the batchfile, e.g., myLevel1Batch.m', required=True, nargs=1)
 
 parser.add_argument('-s','--subjectid', help='the name of the subject(s)', required=False, nargs='*')
 parser.add_argument('-f','--subjectfile', help='the name of the file containing subject(s)', required=False, nargs=1)
 
-parser.add_argument('-n','--batchname', help='The name of the batchfile, e.g., myLevel1Batch.m', required=True, nargs=1)
 
 args = vars(parser.parse_args())
 
@@ -20,9 +20,9 @@ else:
     raise Exception("Must specify either a subject file (-f) or subjectid (-s)")
     
 runcmd = 'matlab -nodisplay -r "try; genL1PostArt('
-runcmd += "\'"+args["basedir"][0]+"\'"
+runcmd += "\'"+args["path"][0]+"\'"
 runcmd += ','+substr+','
-runcmd += "{\'"+args["batchname"][0]+"\'}"
+runcmd += "{\'"+args["template"][0]+"\'}"
 runcmd += ')'
 runcmd += '; catch ME; disp(ME.message);'
 runcmd += "end; exit()\""
