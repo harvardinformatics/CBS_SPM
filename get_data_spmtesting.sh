91d7827e4eaa826f27c827838e87fbdefc87e597
Stephanie McMains <smcmains@fas.harvard.edu>
2015-05-21 14:19:47 -0400

    committing after long time, with arcget update and slurm attempt

---
+++ get_data_spmtesting.sh
@@ -0,0 +1,52 @@
+#!/bin/tcsh
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110808_12v32vis_SubjA_12ch -b 17 21 32 36 47 51 -t 5 -n"
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110808_12v32vis_SubjA_32ch -b 13 17 32 40 51 55 -t 5 -n"
+
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/shared/spm_testing_datasets -s 110811_12v32vis_SubjB_12ch -b 12 16 27 35 46 50 -t 4 -n"
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110811_12v32vis_SubjB_32ch -b 13 17 32 43 47 -t 5 -n"
+
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110812_12v32vis_SubD_12ch -b 12 16 27 31 42 46 -t 4 -n"
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110812_12v32vis_SubD_32ch -b 12 16 27 31 42 46 -t 4 -n"
+
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110815_12v32vis_SubE_12ch -b 13 17 28 32 43 47 -t 5 -n"
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110815_12v32vis_SubE_32ch -b 23 27 38 42 53 57 -t 4 -n"
+
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110818_12v32vis_SubF_12ch -b 12 16 27 31 42 46 -t 4 -n"
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110818_12v32vis_SubF_32ch -b 12 16 27 31 42 46 -t 4 -n"
+
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110909_12v32vis_SubH_12ch -b 12 16 27 31 42 46 -t 4 -n"
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110909_12v32vis_SubH_32ch -b 16 20 31 35 46 50 -t 4 -n"
+
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110912_12v32vis_SubI_12ch -b 12 16 27 31 42 46 -t 4 -n"
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110912_12v32vis_SubI_32ch -b 12 16 27 31 42 46 -t 4 -n"
+
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110919_12v32vis_SubJ_12ch -b 12 16 27 31 42 46 -t 4 -n"
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110919_12v32vis_SubJ_32ch -b 23 27 38 42 53 57 -t 4 -n"
+
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110929_12v32vis_SubK_12ch -b 12 16 27 31 42 46 -t 4 -n"
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110929_12v32vis_SubK_32ch -b 12 16 27 31 42 46 -t 4 -n"
+
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110930_12v32vis_SubL_12ch -b 12 16 27 31 42 46 -t 4 -n"
+
+bsub -q ncf "python /users/mcmains/mri01_users/mcmains/spm_scripting/CBS/getSubjectSPM.py -p ~/mri01_users/mcmains/shared/spm_testing_datasets -s 110930_12v32vis_SubL_32ch -b 12 16 27 31 42 46 -t 4 -n"
+
+
